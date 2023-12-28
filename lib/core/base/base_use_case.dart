194d72aadc7930eacd7a8cc766a63e141c4c87b0
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2023-12-28 19:00:41 +0200

    [feat]:implementation of base use case with result pacakge and add new file for all enums of application

---
+++ lib/core/base/base_use_case.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+import 'package:multiple_result/multiple_result.dart';
+import 'package:trucks/core/errors.dart';
+
+/// Base class for all use cases
+/// [Type] is the return type of the use case
+///  [Params] is the type of parameter passed
+
+///
+/// For example, if the use case is to get a currency history,
+/// then [Type] will be [CurrencyHistoryListEntity] and [Params]
+/// will be [CurrencyHistoryRequestEntity]
+///
+
+abstract interface class BaseUseCase<Type, Params> {
+  /// Executes the use case
+  ///
+  /// [params] is the parameter to be passed to the use case
+  /// Returns the result of the use case
+  ///
+  /// This is callable class so that it can be called directly
+  /// without having to create an instance of the class
+  /// example: GetCurrencyListUseCase() instead of GetCurrencyListUseCase().call()
+  ///
+
+  Future<Result<Type, Failure>> call(Params params);
+}
+
+/// That NoParams class call when there is no any parameter call in any request
+class NoParams extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
