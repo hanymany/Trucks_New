9f5d80627549bbdece508c342185a896d8cd20d1
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2023-12-28 19:01:09 +0200

    [fix]:fix dio class as it never return null

--- lib/core/dio.dart
+++ lib/core/dio.dart
@@ -11,28 +11,28 @@ class MainDio {
     );
   }
 
-  static Future<Response?> getData({
-   required String? path,
+  static Future<Response> getData({
+   required String path,
     Map<String, dynamic>? query,
   }) async {
-    return await dio?.get(path!, queryParameters: query);
+    return await dio!.get(path, queryParameters: query);
   }
 
-  static Future<Response?> postData({
+  static Future<Response> postData({
    required String? path,
     Map<String, dynamic>? query,
    required Map<String, dynamic>? data,
     String? language = 'en',
     String? token,
   }) async {
-    return await dio?.post(path!, queryParameters: query, data: data);
+    return await dio!.post(path!, queryParameters: query, data: data);
   }
 
-  static Future<Response?> putData({
+  static Future<Response> putData({
    required String? path,
     Map<String, dynamic>? query,
    required Map<String, dynamic>? data,
   }) async {
-    return await dio?.put(path!, queryParameters: query, data: data);
+    return await dio!.put(path!, queryParameters: query, data: data);
   }
 }
