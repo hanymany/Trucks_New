1d7050c6b1d17c99545643abc1e043df9c3cc0ea
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2023-12-28 19:00:04 +0200

    [feat]:add new file for all endpoints

---
+++ lib/core/const/end_points/trucks_end_points.dart
@@ -0,0 +1,6 @@
+class TrucksEndPoints{
+  //Todo://Added Right End points
+  static const String adminLogin='adminLogin';
+  static const String driverLogin='adminLogin';
+  static const String agentLogin='adminLogin';
+}
